b7e56aa7d4012bde50d1b41c37afce0969ad0f4a
mohdfaizmaik <9696faiz@gmail.com>
2021-08-28 02:25:23 +0530

    local database added

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,25 @@
+import { useState,useEffect } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState(null)
+    const [isLoading, setisLoading] = useState(true)
+
+    useEffect(()=>{
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setBlogs(data)
+            setisLoading(false)
+        })
+
+    })
     return ( 
         <div className="Home">
-            <h2>Home Page</h2>
+            {isLoading && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
      );
 }
