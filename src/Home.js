a317b15ebb6efc010a27d37dbfb06eeddef64cec
mohdfaizmaik <9696faiz@gmail.com>
2021-08-29 15:56:36 +0530

    add

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,13 @@
 import { useState,useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
-    const [isLoading, setisLoading] = useState(true)
+    const {data: blogs, isLoading, error} = useFetch('http://localhost:8000/blogs')
 
-    useEffect(()=>{
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            setBlogs(data)
-            setisLoading(false)
-        })
-
-    })
     return ( 
         <div className="Home">
+            {error && <div>{error}</div>}
             {isLoading && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
---
+++ src/useFetch.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react"
+
+const useFetch=(url) => {
+    const [data, setData] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(()=>{
+        fetch(url)
+        .then(res => {
+            if(!res.ok){
+                throw Error('could not fetch the data')
+            }
+            return res.json();
+        })
+        .then(data => {
+            setData(data)
+            setIsLoading(false)
+            setError(null)
+        })
+        .catch(err => {
+            setIsLoading(false)
+            setError(err.message)
+        })
+
+    },[])
+
+    return { error, isLoading, data}
+}
+
+export default useFetch;
