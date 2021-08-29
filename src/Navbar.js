4aef787548c44e0628a8537853c14463ed0ea331
mohdfaizmaik <9696faiz@gmail.com>
2021-08-29 17:09:54 +0530

    i have created blog details component

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,9 +3,9 @@ import {Link} from 'react-router-dom';
 const Navbar = () => {
     return ( 
         <nav className="navbar">
-            <h1>My Blog</h1>
+            <Link to="/"><h1>My-Blogs</h1></Link>
             <div className="links">
-                <Link tp="/">Home</Link>
+                <Link to="/">Home</Link>
                 <Link to="/create">New Blog</Link>
             </div>
         </nav>
